bfbf9876fbafa6fa5ac9071cbc74dff66e883d28
Marvin Simon Ohland <uyoso@student.kit.edu>
2025-04-07 14:16:01 +0200

    Remove unnecessary .DS_Store and compiled Python files from the repository

--- main.py
+++ main.py
@@ -6,23 +6,38 @@ def main():
    
     # Here, access to the Raspberry Pi is established, and the picture is taken.
     # The picture is then saved locally here with the name my_filename
-
-    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    
+    
     number= str(input("[INPUT] Please enter the GW number: "))
-    version = str(input("[INPUT] Please enter the version of the GW: "))
-    picture_name =f"GW_{number}_{version}_{current_time}.jpg"
+    scenario = str(input("[INPUT] Please enter the scenario: "))
     
-    make_and_send_picture(
-        hostname="172.22.132.18",
-        username="pirag",
-        password="Omni123",
-        my_filename=picture_name,
-        remote_command_template="""
-        cd /home/pirag/MAA
-        source .venv/bin/activate
-        python camera_activation.py {filename}""")
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     
-    crop_picture(picture_name)
+    while( True ):
+        #stopping = str(input("[INPUT] Do you want to stop the program? (yes/no): "))
+        #if stopping =="y":
+         #       break
+         #v1: normal picture (oben 0,075 // unten 4)
+         #v2: normal picture + rotation
+         #v3: angled gear wheel
+         #v4: gear wheel in hand
+         #v5: no light
+         #v6: no light + correct optical lightning (oben 0,075  // unten 2,4)
+         #v7: blurred (oben 0,225  // unten 4)
+         
+        version = str(input("[INPUT] Please enter the version of the GW: "))
+        picture_name =f"{current_time}__GW_{number}_{scenario}_{version}.jpg"
+        make_and_send_picture(
+            hostname="172.22.132.18",
+            username="pirag",
+            password="Omni123",
+            my_filename=picture_name,
+            remote_command_template="""
+            cd /home/pirag/MAA
+            source .venv/bin/activate
+            python camera_activation.py {filename}""")
+        
+        crop_picture(picture_name)
 
 
 if __name__ == "__main__":
